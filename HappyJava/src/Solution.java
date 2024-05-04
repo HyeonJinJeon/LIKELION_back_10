920464841e9cd2a6850f8f5d219f53b4c8bbb1a6
h_jinnny <nd3807@naver.com>
2024-05-04 21:48:32 +0900

    프로그래머스 디버깅

--- HappyJava/src/Solution.java
+++ HappyJava/src/Solution.java
@@ -1,41 +1,67 @@
 import java.util.*;
-class Solution {
-    static boolean solution(String s) {
-        boolean answer = true;
-        Stack<String> stack = new Stack<>();
 
-        String[] arr = s.split("");
-
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i].equals("{")){
-                stack.push("{");
-            }else {
-                if(stack.isEmpty() == true){
-                    answer = false;
-                }else{
-                    stack.pop();
-                }
+class Solution implements Comparator<String>{
+    public static void main(String args[]) throws Exception {
+//        Scanner sc = new Scanner(System.in);
+//        int T;
+//        T = sc.nextInt();
+//		/*
+//		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+//		*/
+//        String s = "";
+//        ArrayList<String> list = new ArrayList<>();
+//        list.add(s);
+//        s.split(" ");
+//        s.s
+//
+//        for (int test_case = 1; test_case <= T; test_case++) {
+//            int n = sc.nextInt();
+//            int[] price = new int[n];
+//            long remain = 0;
+//
+//            for (int j = 0; j < n; j++) {
+//                price[j] = sc.nextInt();
+//            }
+//            for (int i = 0; i < n; i++) {
+//                int max = price[i];
+//                for (int j = i; j < n; j++) {
+//                    if (price[j] > max){
+//                        max = price[j];
+//                    }
+//                }
+//                remain += (max - price[i]);
+//            }
+//            System.out.println("#"+ test_case + " " + remain);
+//        }
+        solution("500220839878", "7");
+    }
+    public static int solution(String t, String p) {
+        int answer = 0;
+        int compare = 0;
+        StringBuilder stringBuilder = new StringBuilder("aaaaaaaa");
+        String[] str = new String[stringBuilder.length()];
+        Map<String, Integer> map = new HashMap<>();
+        map.put("a", 1);
+        map.get("a");
+        System.out.println(map.get("b"));
+//        Arrays.sort(str, );
+        for(int i = 0; i < t.length()-p.length()+1; i++){
+            compare = (Integer.parseInt(t.substring(i,i+p.length())));
+            if(compare < Integer.parseInt(p)){
+                answer++;
             }
         }
-        if(stack.isEmpty() == false){
-            answer = false;
-        }
+//        Arrays.sort(str, new Comparator<String>() {
+//            @Override
+//            public int compare(String s1, String s2) {
+//
+//            }
+//        });
         return answer;
     }
 
-    public static void main(String[] args) {
-        String polynomial = "3x + 7 + x";
-        int dots[][] = {{1, 2}, {2, 1}, {3, 4}, {4, 5}};
-
-        System.out.println(solution("{{}{"));
-//        String pol = polynomial.replace("\\+", "");
-//        System.out.println(pol);
-//        String[] each = pol.split(" ");
-//        System.out.println(each[0]);
-//        System.out.println(each[1]);
-//        System.out.println(each[2]);
-
+    @Override
+    public int compare(String o1, String o2) {
+        return 0;
     }
 }
-
-
