69d3987026d4a6e320d8d373f6dc4d3cf31dd7fe
h_jinnny <nd3807@naver.com>
2024-05-09 16:03:01 +0900

    Spring 실습

---
+++ HappySpring/hellospring/src/main/java/org/example/hellospring/HellospringApplication.java
@@ -0,0 +1,12 @@
+package org.example.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellospringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HellospringApplication.class, args);
+    }
+
+}
